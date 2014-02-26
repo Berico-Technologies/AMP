9f2bd8ddcb36105e7352936e9fd7550a19b780b5
Jared Holmberg <jholmberg@bericotechnologies.com>
2014-02-26 13:57:21 -0500

    Adding updated configuration for deploying nuget packages.

--- dotnet/source/services/amp.anubis.client/Properties/AssemblyInfo.cs
+++ dotnet/source/services/amp.anubis.client/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("amp.anubis.client")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("AMPere Anubis Client")]
+[assembly: AssemblyDescription("A .NET client to provide greater convenience for connecting with the RESTful Anubis Authentication and Authorization Service")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Berico Technologies")]
 [assembly: AssemblyProduct("amp.anubis.client")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("3.1.3.0")]
+[assembly: AssemblyFileVersion("3.1.3.0")]
